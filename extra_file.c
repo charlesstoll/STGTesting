bfcbabcd93edd174b2f15c5cf74e904221ed74fb
charlesstoll <charlesrstoll@gmail.com>
2018-03-28 20:00:02 -0700

    Initial Commit
     this is my initial commit from normal git in order to start testing stgit

---
+++ extra_file.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int extra_file_print()
+{
+  printf("This is the initial commit\n");
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "extra_file.c"
+
+int main(int argc, char *argv[])
+{
+  extra_file_print();
+  printf("Initial Commit\n");
+
+}
